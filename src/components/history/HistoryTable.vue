d1ae0fd883d578dcaccfd693bb7f4ff5b4aa2aaa
Igor <igorkono343@gmail.com>
2023-02-09 03:45:40 +0200

    history page records table

---
+++ src/components/history/HistoryTable.vue
@@ -0,0 +1,57 @@
+<template>
+	<v-table>
+		<thead>
+			<tr>
+				<th>#</th>
+				<th>Сумма</th>
+				<th>Дата</th>
+				<th>Категория</th>
+				<th>Тип</th>
+				<th>Открыть</th>
+			</tr>
+		</thead>
+
+		<tbody>
+			<tr v-for="(rec, index) in records" :key="rec.id">
+				<td>{{ index+ 1 }}</td>
+				<td>{{ useCurrencyFilter(rec.amount, 'UAH') }}</td>
+				<td>{{ useDateFilter(rec.date, 'datetime') }}</td>
+				<td>{{ rec.category }}</td>
+				<td>
+					<span :class="rec.type === 'income' ? 'bg-red-darken-4' : 'bg-green-darken-2'"
+						class="py-2 px-3 text-white text-center">
+						{{ rec.type === 'income' ? 'доход' : 'расход' }}</span>
+				</td>
+				<td>
+					<v-tooltip :activator="`#rec-${rec.id}`" text="Посмотреть запись" location="bottom"
+						content-class="bg-teal-lighten-2 font-weight-medium">
+						<template v-slot:activator="{ attrs }">
+							<v-btn :id="`rec-${rec.id}`" color="teal-darken-1" @click="router.push('/detail/' + rec.id)">
+								<v-icon icon="mdi-open-in-new" v-bind="attrs" />
+							</v-btn>
+						</template>
+					</v-tooltip>
+				</td>
+			</tr>
+		</tbody>
+	</v-table>
+</template>
+
+<script setup>
+import { useCurrencyFilter } from '@/filters/currencyFilter';
+import { useDateFilter } from '@/filters/dateFilter';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+
+const props = defineProps({
+	records: {
+		required: true,
+		type: Array,
+	}
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/history.vue
+++ src/views/history.vue
@@ -3,49 +3,50 @@
 		<div class="title">
 			<h3 class="text-h4 mt-4 ml-2">История записей</h3>
 		</div>
-		<v-divider color="black" thickness="1.5" class="bg-white mt-1 mb-8" />
-		<div class="history-chart">
+		<v-divider color="black" thickness="1.5" class="bg-white mt-3 mb-6" />
+
+		<div class="history-chart" v-if="!loading">
 			<canvas></canvas>
 		</div>
 
-		<section>
-			<v-table>
-				<thead>
-					<tr>
-						<th>#</th>
-						<th>Сумма</th>
-						<th>Дата</th>
-						<th>Категория</th>
-						<th>Тип</th>
-						<th>Открыть</th>
-					</tr>
-				</thead>
+		<loader v-if="loading" class="mt-7 page-loader" />
 
-				<tbody>
-					<tr>
-						<td>1</td>
-						<td>1212</td>
-						<td>12.12.32</td>
-						<td>name</td>
-						<td>
-							<span class="py-2 px-3 text-white text-center bg-red-darken-4">Расход</span>
-						</td>
-						<td>
-							<v-btn color="teal-darken-1">
-								<v-icon icon="mdi-open-in-new" />
-							</v-btn>
-						</td>
-					</tr>
-				</tbody>
-			</v-table>
+		<div v-else-if="!records.length" class="text-center text-h6 mt-9">Записей пока нет. <router-link
+				to="/record">Создать запись</router-link></div>
+
+		<section v-else class="mt-8">
+			<HistoryTable :records="records" />
 		</section>
 	</div>
 </template>
 
 <script setup>
+import HistoryTable from '@/components/history/HistoryTable.vue';
+import { ref, onMounted } from 'vue';
+import { useStore } from 'vuex';
+
+const store = useStore();
+
+const loading = ref(true);
+const records = ref([]);
+const categories = ref([]);
+
+onMounted(async () => {
+	const rec = await store.dispatch('record/fetchRecords');
+	categories.value = await store.dispatch('category/fetchCategories');
+	records.value = rec.map(r => ({
+		...r,
+		category: categories.value.find(cat => cat.id === r.categoryId).title,
+	}));
+	loading.value = false;
+});
+
 
 </script>
 
 <style lang="scss" scoped>
-
+.page-loader {
+	left: 50%;
+	transform: translate(-50%);
+}
 </style>
