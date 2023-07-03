6128d4d34b046c04f1a9f775aa41967c02e41a63
Igor Kononenko <igorkono343@gmail.com>
2023-07-03 19:39:53 +0300

    Deploying to firebase with github actions

--- dist/assets/home-1feee260.js
+++
@@ -1,1 +0,0 @@
-import{d as s,U as t,j as o}from"./index-906b0197.js";const p=s({__name:"home",setup(a){t({title:"pageTitles.home"});const{push:e}=o();return e("/"),()=>{}}});export{p as default};
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 ///<reference types="vitest"/>
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
 import vue from '@vitejs/plugin-vue';
@@ -10,46 +10,50 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	// base: '/crm-vue/',
-	server: {
-		port: 3000
-	},
-	resolve: {
-		alias: {
-			'@': resolve(dirname(fileURLToPath(import.meta.url)), './src')
-		}
-	},
-	plugins: [
-		vue({ template: { transformAssetUrls } }),
-		VueI18nPlugin({
-			globalSFCScope: true,
-			include: [resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**')]
-		}),
-		Pages({
-			dirs: 'src/views',
-			extendRoute(route) {
-				if (route.path === '/login' || route.path === '/register') {
-					return route;
-				}
-				return {
-					...route,
-					meta: { auth: true }
-				};
+export default ({ mode }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+	return defineConfig({
+		base: process.env.VITE_BASE || '/',
+		server: {
+			port: +process.env.VITE_PORT || 3000
+		},
+		resolve: {
+			alias: {
+				'@': resolve(dirname(fileURLToPath(import.meta.url)), './src')
 			}
-		}),
-		Layouts({
-			layoutsDirs: 'src/layouts',
-			defaultLayout: 'main'
-		}),
-		vuetify()
-	],
-	test: {
-		include: ['**/__tests__/**'],
-		globals: true,
-		environment: 'jsdom',
-		deps: {
-			inline: ['vuetify']
+		},
+		plugins: [
+			vue({ template: { transformAssetUrls } }),
+			VueI18nPlugin({
+				globalSFCScope: true,
+				include: [resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**')]
+			}),
+			Pages({
+				dirs: 'src/views',
+				extendRoute(route) {
+					if (route.path === '/login' || route.path === '/register') {
+						return route;
+					}
+					return {
+						...route,
+						meta: { auth: true }
+					};
+				}
+			}),
+			Layouts({
+				layoutsDirs: 'src/layouts',
+				defaultLayout: 'main'
+			}),
+			vuetify()
+		],
+		test: {
+			include: ['**/__tests__/**'],
+			globals: true,
+			environment: 'jsdom',
+			deps: {
+				inline: ['vuetify']
+			}
 		}
-	}
-});
+	});
+};
